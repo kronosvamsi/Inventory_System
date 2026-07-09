d46907d55c03528d8001dac2b24db7256d210999
kronosvamsi <beingaliver@gmail.com>
2026-07-09 16:24:02 +0530

    Add Base db exception and custom product err

---
+++ exceptions/db_exception.py
@@ -0,0 +1,34 @@
+""" Base DB exception """
+
+from functools import wraps
+from exceptions import ProductNotFoundError
+import logging
+
+def db_exc_handler(func):
+    
+    def get_db_session(*args, **kwargs):
+        if "db" in kwargs:
+            return kwargs['db']
+        for arg in args:
+            if hasattr(arg, "rollback") and callable(getattr(arg, "rollback")):
+                return arg
+        return None
+    
+    def handle_rollback(db_session, error):
+        if db_session:
+            try:
+                logging.warning(f"Rollback Database trnasaction due to error: {str(error)} ")
+                db_session.rollback()
+            except Exception as rollback_err:
+                logging.critical(f"Failed to rollback database: {str(rollback_err)}")
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as err:
+            db_session = get_db_session(*args, **kwargs)
+            handle_rollback(db_session,err)
+            raise err
+    
+    return wrapper
---
+++ exceptions/products_errors.py
@@ -0,0 +1,40 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+class ProductNotFoundError(Exception):
+    def __init__(self, message:str, product_id:int):
+        super().__init__(message)
+        self.message = message
+        self.product_id = product_id
+
+class NegativeQuantityError(Exception):
+    def __init__(self, message:str, quantity :int):
+        self.message  = message
+        self.quantity = quantity
+        super().__init__(message)
+
+def negative_quantity_handler(request:Request, exc:NegativeQuantityError):
+    return JSONResponse(
+        status_code=400,
+        content = {
+            "error":"Negative_Quantity", 
+            "msg":exc.message ,
+            "quantity":exc.quantity
+            }
+        )
+    
+
+async def product_not_found_handler(request:Request, exc:ProductNotFoundError):
+    return JSONResponse( 
+        status_code= 404 ,
+        content = {
+            "error":"PRODUCT_NOT_FOUND",
+            "msg":exc.message, 
+            "product_id":exc.product_id
+            }
+) 
+
+product_handlers = {
+    ProductNotFoundError : product_not_found_handler,
+    NegativeQuantityError : negative_quantity_handler
+}
